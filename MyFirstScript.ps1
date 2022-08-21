9ba009a645d9b322d10e57d1f10d8c1326824198
ANA-NURFARIDAH <anafaism@gmail.com>
2022-08-21 11:27:22 +0700

    first commit

---
+++ MyFirstScript.ps1
@@ -0,0 +1,8 @@
+Windows PowerShell
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+Try the new cross-platform PowerShell https://aka.ms/pscore6
+
+PS C:\Windows\system32> Write-Host "Hello, World!"
+Hello, World!
+PS C:\Windows\system32>
