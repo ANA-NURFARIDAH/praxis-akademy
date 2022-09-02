1ab193e19d9f536b05d0965b7e78d35b6ac3f0fd
ANA-NURFARIDAH <anafaism@gmail.com>
2022-09-02 11:21:05 +0700

    first commit

--- novice/03-05/app/main.py
+++ novice/03-05/app/main.py
@@ -1,19 +1,31 @@
-from urllib import request
 from flask import Flask, render_template, request
+import psycopg2
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
-def index():
+def index(): 
+    conn = psycopg2.connect(
+        host="localhost",
+        database="contoh",
+        user="postgres",
+        password="nurfaridah12345"
+        )
+    curs = conn.cursor()
     if request.method == "POST":
-        print(request.method)
-        print(20*"=")
-        print(request.form)
-        print(request.form.get("nama"))
-        print(request.form.get("detail"))
-        print(20*"=")
+        nama = request.form.get("nama")
+        detail = request.form.get("detail")
+        query = f"insert into buah(nama, detail) values('{nama}', '{detail}')"
+        curs.execute(query)
+        conn.commit()
+        curs.close()
+        conn.close()
+    
+    #     print(20*"=")
+    #     print(nama)
+    #     print(detail)
+    #     print(20*"=")
 
     print(request.method)
-    
     data = ["apel", "pear", "anggur"]
     return render_template("index.html", contex=data)
 
