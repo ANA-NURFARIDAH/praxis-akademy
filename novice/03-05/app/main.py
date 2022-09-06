4d0eb0b4592d0a64a61cd5d5ee3dc4357e3fed98
ANA-NURFARIDAH <anafaism@gmail.com>
2022-09-06 09:02:40 +0700

    first commit

--- novice/03-05/app/main.py
+++ novice/03-05/app/main.py
@@ -3,36 +3,39 @@ import psycopg2
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
-def index(): 
+def index():
     conn = psycopg2.connect(
         host="localhost",
         database="contoh",
         user="postgres",
-        password="nurfaridah12345")
+        password="nurfaridah12345"
+    )
+
     curs = conn.cursor()
     if request.method == "POST":
         nama = request.form.get("nama")
         detail = request.form.get("detail")
-        query = f"insert into buah(nama, detail) values('{nama}', '{detail}')"
+        query = f"insert into buah(nama, detail) values ('{nama}', '{detail}')"
         curs.execute(query)
-        conn.commit()
+        conn.commit()        
         curs.close()
         conn.close()
-    
+        
         # print(20*"=")
         # print(nama)
         # print(detail)
         # print(20*"=")
-
+    
     print(request.method)
     query = f"select * from buah"
     curs.execute(query)
     data = curs.fetchall()
     curs.close()
     conn.close()
-    # data = ["apel", "pear", "anggur"]
+    # data = ["apel", "pear", "anggur", "jeruk", "belimbing", "kelengkeng"]
     return render_template("index.html", context=data)
 
+    
 @app.route("/detail/<buah_id>")
 def detail(buah_id):
     conn = psycopg2.connect(
@@ -45,12 +48,11 @@ def detail(buah_id):
     query = f"select * from buah where id = {buah_id}"
     curs.execute(query)
     data = curs.fetchone()
-    conn.commit()
+    conn.commit()        
     curs.close()
     conn.close()
-    print(data)
-    
-    return render_template("index.html", context=data)
+    print (data)
+    return render_template("detail.html", context=data)
 
 @app.route("/delete/<buah_id>")
 def delete(buah_id):
@@ -63,10 +65,10 @@ def delete(buah_id):
     curs = conn.cursor()
     query = f"delete from buah where id = {buah_id}"
     curs.execute(query)
-    conn.commit()
+    conn.commit()        
     curs.close()
     conn.close()
-    return redirect("/")
+    return redirect ("/")
 
 @app.route("/update/<buah_id>")
 def update(buah_id):
@@ -77,11 +79,17 @@ def update(buah_id):
         password="nurfaridah12345"
     )
     curs = conn.cursor()
-    query = f"update from buah where id = {buah_id}"
+   
+    namaLama = 'bengkoang'
+    namaBaru = 'strobery'
+    detailBaru = 'sepet'
+
+    query = f"update buah set nama='{namaBaru}', detail='{detailBaru}' where nama ='{namaLama}'"
     curs.execute(query)
     conn.commit()
-    curs.close()
-    conn.close()
+    print("data masuk")
+    
     return redirect("/")
+
 if __name__ == "__main__":
     app.run()
