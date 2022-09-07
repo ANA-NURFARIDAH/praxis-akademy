0d6103389f5e8eb14c4b18e469ac124f13ec462d
ANA-NURFARIDAH <anafaism@gmail.com>
2022-09-07 16:56:27 +0700

    first commit

--- novice/04-03/app/main.py
+++ novice/04-03/app/main.py
@@ -17,9 +17,9 @@ def index():
         asal_kabupaten = request.form.get("asal_kabupaten")
         blok_kamar = request.form.get("blok_kamar")
         keterangan = request.form.get("keterangan")
-        query = f"insert into projectku(nama, asal_kabupaten, blok_kamar, keterangan) values ('{nama}', '{asal_kabupaten}', '{blok_kamar}', '{keterangan}'"
+        query = f"insert into projectku(nama, asal_kabupaten, blok_kamar, keterangan) values ('{nama}', '{asal_kabupaten}', '{blok_kamar}', '{keterangan}')"
         curs.execute(query)
-        conn.commit()        
+        conn.commit()  
         
     print(request.method)
     query = f"select * from projectku"
@@ -30,65 +30,65 @@ def index():
     return render_template("index.html", context=data)
 
     
-# @app.route("/detail/<data_santri_putri_programmer_ppqs_id>")
-# def detail(data_santri_putri_programmer_ppqs_id):
-#     conn = psycopg2.connect(
-#         host="localhost",
-#         database="data_santri_putri_programmer_ppqs",
-#         user="postgres",
-#         password="nurfaridah12345"
-#     )
-#     curs = conn.cursor()
-#     query = f"select * from projectku where id = {data_santri_putri_programmer_ppqs_id}"
-#     curs.execute(query)
-#     data = curs.fetchone()
-#     conn.commit()        
-#     curs.close()
-#     conn.close()
-#     print (data)
-#     return render_template("detail.html", context=data)
+@app.route("/detail/<projectku_id>")
+def detail(projectku_id):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="data_santri_putri_programmer_ppqs",
+        user="postgres",
+        password="nurfaridah12345"
+    )
+    curs = conn.cursor()
+    query = f"select * from projectku where id = {projectku_id}"
+    curs.execute(query)
+    data = curs.fetchone()
+    conn.commit()        
+    curs.close()
+    conn.close()
+    print (data)
+    return render_template("detail.html", context=data)
 
-# @app.route("/delete/<data_santri_putri_programmer_ppqs_id>")
-# def delete(data_santri_putri_programmer_ppqs_id):
-#     conn = psycopg2.connect(
-#         host="localhost",
-#         database="data_santri_putri_programmer_ppqs",
-#         user="postgres",
-#         password="nurfaridah12345"
-#     )
-#     curs = conn.cursor()
-#     query = f"delete from projectku where id = {data_santri_putri_programmer_ppqs_id}"
-#     curs.execute(query)
-#     conn.commit()        
-#     curs.close()
-#     conn.close()
-#     return redirect ("/")
+@app.route("/delete/<projectku_id>")
+def delete(projectku_id):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="data_santri_putri_programmer_ppqs",
+        user="postgres",
+        password="nurfaridah12345"
+    )
+    curs = conn.cursor()
+    query = f"delete from projectku where id = {projectku_id}"
+    curs.execute(query)
+    conn.commit()        
+    curs.close()
+    conn.close()
+    return redirect ("/")
 
-# @app.route("/update/<data_santri_putri_programmer_ppqs_id>" ,methods=["GET", "POST"])
-# def update(data_santri_putri_programmer_ppqs,_id):
-#     conn = psycopg2.connect(
-#         host="localhost",
-#         database="data_santri_putri_programmer_ppqs",
-#         user="postgres",
-#         password="nurfaridah12345"
-#     )
-#     curs = conn.cursor()
-#     if request.method == "POST":
-#         nama = request.form.get("nama")
-#         detail = request.form.get("detail")
-#         query = f"update projectku set nama='{nama}', asal_kabupaten='{detail}', blok_kamar='{detail}', keterangan='{detail}' where nama ='{nama}'"
- 
+@app.route("/update/<projectku_id>" ,methods=["GET", "POST"])
+def update(projectku_id):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="data_santri_putri_programmer_ppqs",
+        user="postgres",
+        password="nurfaridah12345"
+    )
+    curs = conn.cursor()
+    if request.method == "POST":
+        nama = request.form.get("nama")
+        asal_kabupaten = request.form.get("asal_kabupaten")
+        blok_kamar= request.form.get("blok_kamar")
+        keterangan = request.form.get("keterangan")
+        query = f"update projectku set nama = '{nama}', asal_kabupaten = '{asal_kabupaten}', blok_kamar = '{blok_kamar}', keterangan = '{keterangan}' where id = {projectku_id}"
+        curs.execute(query)
+        conn.commit()
+        return redirect("/")
 
-#         curs.execute(query)
-#         conn.commit()
-#         return redirect("/")
-
-#     query = f"select * from projectku where id = {data_santri_putri_programmer_ppqs,_id}"
-#     curs.execute(query)
-#     data = curs.fetchone()
-#     conn.close()
-#     conn.close()
-#     return render_template("update.html", context=data)
+    query = f"select * from projectku where id = {projectku_id}"
+    curs.execute(query)
+    data = curs.fetchone()
+    conn.close()
+    conn.close()
+    return render_template("update.html", context=data)
 
 if __name__ == "__main__":
     app.run()
